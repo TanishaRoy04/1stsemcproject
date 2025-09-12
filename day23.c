55fc8f2b53b40f8a57ea049219bbc7aac6d06834
TanishaRoy04 <rtanisha065@gmail.com>
2025-09-12 23:42:34 +0530

    Add files via upload

---
+++ day23.c
@@ -0,0 +1,39 @@
+/*Q46: Write a program to print the following pattern:
+*****
+*****
+*****
+*****
+*****
+*/
+
+
+/*
+Sample Test Cases:
+Input 1:
+
+Output 1:
+*****
+*****
+*****
+*****
+*****
+
+*/
+
+
+#include <stdio.h>
+
+int main() {
+    int i, j;
+    for (i = 0; i < 5; i++) {          // Loop for rows
+        for (j = 0; j < 5; j++) {      // Loop for columns
+            printf("*");
+        }
+        printf("\n");                  // Move to next line
+    }
+    return 0;
+
+}
+
+
+
