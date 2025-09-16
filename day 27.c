4caf999981288c953b511436f0c87462b0edc38a
TanishaRoy04 <rtanisha065@gmail.com>
2025-09-16 21:35:02 +0530

    Add files via upload

---
+++ day 27.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int i, j;
+
+    // upper half (1,3,5,7,9 stars)
+    for (i = 1; i <= 9; i += 2) {
+        for (j = 1; j <= i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    // lower half (7,5,3,1 stars)
+    for (i = 7; i >= 1; i -= 2) {
+        for (j = 1; j <= i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
